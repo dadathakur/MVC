013d0d295140fdd3f5e84d32c56fc30bb56a8988
dadathakur <dshershah@gmail.com>
2014-06-18 22:35:49 -0400

    testing

---
+++ New_MVC_app/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(New_MVC_app.Startup))]
+namespace New_MVC_app
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
